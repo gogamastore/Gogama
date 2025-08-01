5aa47bfca439c7a9bcc7718c7048b461a1dff86c
Ga Ming <reza.phv31@gmail.com>
2025-08-01 06:33:54 +0000

    dimana saya menemukan firebase secret api key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCThjETkAH8HpFoclhmXtgjm2lIr1506dg",
-  authDomain: "orderflow-r7jsk.firebaseapp.com",
-  projectId: "orderflow-r7jsk",
-  storageBucket: "orderflow-r7jsk.firebasestorage.app",
-  messagingSenderId: "954515661623",
-  appId: "1:954515661623:web:19d89bf3722600e02ef0b2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Inisialisasi Firebase hanya sekali
