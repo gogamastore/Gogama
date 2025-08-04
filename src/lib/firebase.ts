5f9ee0cfaf580ad6e012c441a90abad45450f392
Ga Ming <reza.phv31@gmail.com>
2025-08-04 19:52:53 +0000

    tapi pada firebase config pada proyek ini, ini yang saya miliki di proye

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  "projectId": "orderflow-r7jsk",
-  "appId": "1:954515661623:web:cde7b76e569ebab32ef0b2",
-  "storageBucket": "orderflow-r7jsk.firebasestorage.app",
-  "apiKey": "AIzaSyAoSTtdMU938EgQ0KoaHpCKXMVUOebU0I8",
-  "authDomain": "orderflow-r7jsk.firebaseapp.com",
-  "messagingSenderId": "954515661623"
+  apiKey: "AIzaSyCThjETkAH8HpFoclhmXtgjm2lIr1506dg",
+  authDomain: "orderflow-r7jsk.firebaseapp.com",
+  projectId: "orderflow-r7jsk",
+  storageBucket: "orderflow-r7jsk.firebasestorage.app",
+  messagingSenderId: "954515661623",
+  appId: "1:954515661623:web:19d89bf3722600e02ef0b2"
 };
 
 // Initialize Firebase using a singleton pattern
