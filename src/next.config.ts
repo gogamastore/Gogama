abf2cb29fabbef2262e8c33258866d89b1509027
Ga Ming <reza.phv31@gmail.com>
2025-08-05 18:46:49 +0000

    sobat saya membuat proyek baru untuk capacitor di workspace berbeda di p

---
+++ src/next.config.ts
@@ -0,0 +1,36 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true, 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+        '*.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev'
+    ],
+  },
+};
+
+export default nextConfig;
