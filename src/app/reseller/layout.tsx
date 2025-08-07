41120a839aa94f7a34b432f0f75ea95717298a9c
Ga Ming <reza.phv31@gmail.com>
2025-08-07 14:53:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/reseller/layout.tsx
+++ src/app/reseller/layout.tsx
@@ -1,8 +1,6 @@
 
 import type { Metadata } from "next";
-import { Poppins, PT_Sans } from "next/font/google";
 import "./../globals.css";
-import { cn } from "@/lib/utils";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import { Toaster } from "@/components/ui/toaster";
@@ -24,24 +22,15 @@ export default function ResellerLayout({
   children: React.ReactNode;
 }>) {
   return (
-     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className={cn("font-body antialiased")}>
-        <CartProvider>
-            <div className="relative flex min-h-dvh flex-col bg-background">
-              <Header />
-              <main className="flex-1 pb-20 md:pb-0">{children}</main>
-              <Footer />
-              <BottomNav />
-              <ResellerChatTrigger />
-            </div>
-            <Toaster />
-        </CartProvider>
-      </body>
-    </html>
+    <CartProvider>
+        <div className="relative flex min-h-dvh flex-col bg-background">
+          <Header />
+          <main className="flex-1 pb-20 md:pb-0">{children}</main>
+          <Footer />
+          <BottomNav />
+          <ResellerChatTrigger />
+        </div>
+        <Toaster />
+    </CartProvider>
   );
 }
